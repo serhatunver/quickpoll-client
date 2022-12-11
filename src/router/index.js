d4e21d97cee772239a57330230edacadf06eff2d
unverserhat <96500903+unverserhat@users.noreply.github.com>
2022-12-11 19:13:56 +0300

    fix naming

--- src/router/index.js
+++ src/router/index.js
@@ -1,8 +1,5 @@
 import { createRouter, createWebHistory } from "vue-router";
 import MainView from "../views/MainView.vue";
-// import VoteView from "../views/VoteView.vue";
-// import ResultsView from "../views/ResultView.vue";
-// import NewForm from "../views/NewForm.vue";
 import NotFoundView from "../views/NotFoundView.vue";
 
 const router = createRouter({
@@ -11,26 +8,22 @@ const router = createRouter({
     {
       path: "/",
       name: "main",
-      // component: () => import("../views/MainView.vue"),
       component: MainView,
     },
     {
       path: "/poll/:id",
       name: "vote",
       component: () => import("../views/VoteView.vue"),
-      // component: VoteView,
     },
     {
       path: "/result/:id",
       name: "result",
       component: () => import("../views/ResultView.vue"),
-      // component: ResultsView,
     },
     {
       path: "/create",
       name: "create",
-      component: () => import("../views/NewForm.vue"),
-      // component: NewForm,
+      component: () => import("../views/CreatePollView.vue"),
     },
     {
       path: "/:pathMatch(.*)*",
